c5a5df98dd6ca0a8485e1b2e9702bc6678852e19
Егор Чувашов <wowonline.fit@mail.ru>
2022-04-15 20:04:25 +0300

    strated working w/ templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
-@app.route('/jopa')
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello world!'
+    user = {'username': 'Egor'}
+    posts = [
+        {
+            'author': {'username': 'Chmoshnik'},
+            'body': 'Ya uchus` na vmike! I eto kruto'
+        },
+        {
+            'author': {'username': 'Nikita'},
+            'body': 'Ya takoy koncheniy!'
+        },
+        {
+            'author': {'username': 'Yarik'},
+            'body': 'Nado vcativat`sya v rast!!!!'
+        }
+    ]
+    return render_template('index.html', title='huy', user=user, posts=posts)
